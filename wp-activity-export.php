12a0476004785b84fb9edc359b21f0ac326d8125
Dric1107 <Dric1107@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-26 14:11:39 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-activity/trunk@480745 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-activity-export.php
@@ -0,0 +1,92 @@
+<?php
+
+$wpcontentdir = "wp-content"; //You have to change this if you renamed your wp-content directory.
+
+
+$script_filename = $_ENV["SCRIPT_FILENAME"];
+$cut = strpos($script_filename, "/".$wpcontentdir."/plugins/");
+$path_tab = str_split($script_filename, $cut);
+require($path_tab[0]."/wp-blog-header.php");
+
+header("Content-Type: application/csv-tab-delimited-table");
+header("Content-disposition: filename=wp-activity.csv");
+
+if (current_user_can('administrator')){
+  function act_export(){
+    global $wpdb;
+    if(isset($_POST['act_export']) and check_admin_referer('wp-activity-export','act_export_csv')){
+      $act_sqlorderby_sec = '';
+      if (isset($_POST['act_type_filter'])){
+        $act_type_filter = esc_html($_POST['act_type_filter']);
+        if ($act_type_filter <> 'all'){
+          $sqlfilter = 'AND act_type = "'.$act_type_filter.'"';
+        }
+      }else{
+        $sqlfilter = '';
+      }
+      if (isset($_POST['act_order_by'])){
+        $act_order_by = esc_html($_POST['act_order_by']);
+      }else{
+      	$act_order_by = 'order_date';
+      }
+    
+      switch ($act_order_by) {
+      	case 'order_user' :
+      		$sqlorderby = 'display_name';
+      		$sqlasc = 'ASC';
+          $act_sqlorderby_sec = ', act_date DESC';
+      		break;
+      	case 'order_type' :
+      		$sqlorderby = 'act_type';
+      		$sqlasc = 'ASC';
+          $act_sqlorderby_sec = ', act_date DESC';
+      		break;
+      	case 'order_date' :
+      	default :
+      		$sqlorderby = 'act_date';
+      		$sqlasc = 'DESC';
+      		break;
+      }
+      $act_recent_sql  = "SELECT * FROM ".$wpdb->prefix."activity AS activity, ".$wpdb->prefix."users AS users WHERE activity.user_id = users.id ".$sqlfilter." ORDER BY ".$sqlorderby." ".$sqlasc." ".$act_sqlorderby_sec;
+      if ( $logins = $wpdb->get_results($wpdb->prepare($act_recent_sql))){
+        echo __("Date", 'wp-activity').';'.__("User", 'wp-activity').';'.__("Event Type", 'wp-activity').';'.__("Applies to", 'wp-activity').";\n";
+        foreach ( (array) $logins as $act ){
+           echo $act->act_date.';';
+           switch ($act->act_type){
+            case 'LOGIN_FAIL' :
+              echo $act->act_params.';'.$act->act_type.';;';
+              break;
+            case 'CONNECT':
+              echo $act->display_name.';'.$act->act_type.';;';
+              break;
+            case 'COMMENT_ADD':
+              $act_comment=get_comment($act->act_params);
+              $act_post=get_post($act_comment->comment_post_ID);
+              echo $act->display_name.';'.$act->act_type.';'.$act_post->post_title.';';
+              break;
+            case 'POST_ADD':
+            case 'POST_EDIT':
+              $act_post=get_post($act->act_params);
+              echo $act->display_name.';'.$act->act_type.';'.$act_post->post_title.';';
+              break;
+            case 'PROFILE_EDIT':
+              echo $act->display_name.';'.$act->act_type.';;';
+              break;
+            case 'LINK_ADD':
+              $link = get_bookmark($act->act_params);
+              if ($link->link_visible == 'Y'){
+                echo $act->display_name.';'.$act->act_type.';'.$link->link_name.';';
+              }
+              break;
+            default:
+              break;
+          }
+          echo "\n"; 
+        }  
+      }
+      $wpdb->print_error();
+    }
+  }
+  act_export();
+}  
+?>
